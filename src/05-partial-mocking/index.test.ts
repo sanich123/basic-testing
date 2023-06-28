683c5abd7e2a49291bb0578ff4d1eb6d18c5c065
Alexander Voronin <aovoronin.piano@gmail.com>
2023-06-28 23:26:11 +0300

    feat: solve 05-partial-mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,26 @@
-// Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
-
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  return {
+    __esModule: true,
+    ...jest.requireActual<typeof import('./index')>('./index'),
+    mockOne: 'some fucking string',
+    mockTwo: 'another fucking string',
+    mockThree: 666,
+  };
 });
 
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+
 describe('partial mocking', () => {
-  afterAll(() => {
-    jest.unmock('./index');
-  });
+  afterAll(() => jest.unmock('./index'));
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    expect(mockOne).toEqual('some fucking string');
+    expect(mockTwo).toEqual('another fucking string');
+    expect(mockThree).toEqual(666);
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    unmockedFunction();
+    //to see this - npm run test:verbose
   });
 });
